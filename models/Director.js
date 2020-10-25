5ee15b7c700eb2cac29e637a284f4244179a1852
TheSopranos <zestillest@gmail.com>
2020-10-25 18:57:05 +0300

    Directors and Movies Validation

--- models/Director.js
+++ models/Director.js
@@ -1,17 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// VALİDASYON İŞLEMLERİ SONRADAN YAPILDI
 const DirectorSchema = new Schema ({
    name: {
        type: String,
+       maxlength: 30,
+       minlength: 2,
        required: true,
    },
    surname: {
        type: String,
+       maxlength: 30,
+       minlength: 2,
        required: true
    },
    bio: {
-       type: String
+       type: String,
+       maxlength: 1000,
+       minlength: 60
    },
    createdAt: {
        type: Date,
--- models/Movie.js
+++ models/Movie.js
@@ -1,15 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// VALİDASYON İŞLEMLERİ SONRADAN YAPILDI
 const MovieSchema = new Schema ({
    director_id: Schema.Types.ObjectId,
    title: {
        type: String,
-       required: true
+       required: [true, '`{PATH} alanı zorunludur.`'],
+       maxlength: [15, '`{PATH}` alanı `{MAXLENGTH} karakterden fazla olamaz.`'],
+       minlength: [1, '`{PATH}` alanı `{MAXLENGTH}` karakterden büyük olmalıdır. '],
    },
-   category: String,
-   country: String,
-   year: Number,
+   category: {
+       type: String,
+       maxlength: 30,
+       minlength: 4
+   },
+   country: {
+       type: String,
+       maxlength: 30,
+       minlength: 2
+   },
+   year: {
+       type: Number,
+       max: 2040,
+       min: 1900
+   },
    imdb_score: {
        type: Number,
        max: 10,
