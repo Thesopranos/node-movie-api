7679dee29cb3f4f2bd97f00696d28c0ace74eeb5
TheSopranos <zestillest@gmail.com>
2020-11-11 01:24:50 +0300

    /api/movies [POST] unit test

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -37,4 +37,32 @@ describe('/api/movies tests', ( ) => {
            })
        });
     });
+
+    describe('/POST movie', () => {
+        it('it should POST a movie ', (done) => {
+            const movie = { // burda bir örnek obje oluşturduk film post edebilmek için
+                title: 'Udemy',
+                director_id: '5f957d9300078c0e1c14f633',
+                category: 'comedy',
+                country: 'Turkey',
+                year: 1950,
+                imdb_score: 8
+            }
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie)
+                .set('x-access-token', token) // burda tokeni verdik
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object'); // burda obje olmasını kontrol ettik
+                    res.body.should.have.property('title'); // burdan sonraki kısımda tek tek propertylerin varlığını kontrol ettik
+                    res.body.should.have.property('director_id'); // eksik property olmaması için
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    done(); // asıl profesyonel olan test db'si oluşturup orada test etmek
+                }) // bu şu an post ediyor direkt sürekli db'ye ekliyor ileride testten sonra silmesini de sağlayacağız
+        })
+    })
 });
